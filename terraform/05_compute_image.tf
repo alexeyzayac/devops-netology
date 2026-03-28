02d0552e3ade8f1e62e32d2e8e80ea9a993f1ec0
alexeyzayac <alexeyzayac@icloud.com>
2026-03-28 17:40:39 +0300

    15.03_docker_compose_orchestration

--- terraform/05_compute_image.tf
+++
@@ -1,9 +0,0 @@
-# 05_compute_image.tf
-locals {
-  image_id = "fd8stsue5rim479kphah"
-}
-
-# либо data для справки, но это необязательно
-data "yandex_compute_image" "toolbox" {
-  image_id = local.image_id
-}
